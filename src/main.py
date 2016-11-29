ff942ab42b8401bcf2b3a707fd59b7e02a843e99
Louis THIRY <louis.thiry@outlook.fr>
2016-11-29 12:44:31 +0100

    feat(main): create a function that implement KNN on a pollutant

--- src/main.py
+++ src/main.py
@@ -31,34 +31,42 @@ PM2_5values = train.loc[train['pollutant'] == 'PM2_5']['TARGET'].values
 # afficher un histogramme
 plotUtils.plot_histogram(NO2values, 'NO2');
 
-# methode plus proches voisins pour le NO2
+# methode plus proches voisins
 
-# preprocessing
-# on recupere les valeurs du NO2
-train_NO2 = train.loc[train['pollutant'] == 'NO2'].reset_index(drop = True).drop('pollutant', axis = 1);
-# on complete les valeurs non renseignées (nécessaire pour l'algo)
-fill_NaN = Imputer(missing_values = np.nan);
-train_NO2_fill = pd.DataFrame(fill_NaN.fit_transform(train_NO2));
-train_NO2_fill.columns = train_NO2.columns;
+def nearestNeighbour(pollutant, n_neighbors, train_data, test_data):
+    # preprocessing
+    # on recupere les valeurs du polluant, on enleve les colonne qui n'ont que Nan, on remplace les NaN par la moyenne, on separe X et Y et on enleve l'ID
+    train = train_data.loc[train_data['pollutant'] == pollutant].reset_index(drop = True).drop('pollutant', axis = 1).dropna(axis = 1, how = 'all');
+    fill_NaN = Imputer(missing_values = np.nan);
+    train_fill = pd.DataFrame(fill_NaN.fit_transform(train));
+    train_fill.columns = train.columns;
 
-# idem pour les données de test
-X_test_NO2 = X_test.loc[X_test['pollutant'] == 'NO2'].reset_index(drop = True).drop('pollutant', axis = 1);
-X_test_NO2_fill = pd.DataFrame(fill_NaN.fit_transform(X_test_NO2));
-X_test_NO2_fill.columns = X_test_NO2.columns;
+    train_X_preproc = train_fill.drop('TARGET', axis = 1).drop('ID', axis = 1);
+    train_Y_preproc = train_fill['TARGET']
+
+    # idem pour les données de test
+    test_X = test_data.loc[test_data['pollutant'] == pollutant].reset_index(drop = True).drop('pollutant', axis = 1).dropna(axis = 1, how = 'all');
+
+    test_X_fill = pd.DataFrame(fill_NaN.fit_transform(test_X));
+    test_X_fill.columns = test_X.columns;
+    test_X_preproc = test_X_fill.drop('ID', axis = 1);
 
 
-# paramétrage de la méthode plus proche voisins
-n_neighbors = 5
-weights  = 'uniform' # autre valeur possible 'distance'
-knn = neighbors.KNeighborsRegressor(n_neighbors, weights=weights)
+    # paramétrage de la méthode plus proche voisins
+    weights = 'uniform' # autre valeur possible 'distance'
+    knn = neighbors.KNeighborsRegressor(n_neighbors, weights = weights)
 
-X = train_NO2_fill.drop('TARGET', axis = 1).drop('ID', axis = 1);
-Y = train_NO2_fill['TARGET']
+    # methode plus proche voisins
+    fittedKnn = knn.fit(train_X_preproc, train_Y_preproc);
+    test_Y_predicted = pd.DataFrame(fittedKnn.predict(test_X_preproc));
+    test_Y_predicted.columns = ['TARGET'];
+    return pd.concat([test_X['ID'], test_Y_predicted], axis = 1);
 
-fittedKnn = knn.fit(X, Y);
-Y_predicted = pd.DataFrame(fittedKnn.predict(X_test_NO2_fill.drop('ID', axis = 1)));
-Y_predicted.columns = ['TARGET'];
-Y_test_NO2 = pd.concat([X_test_NO2['ID'], Y_predicted], axis = 1);
+# methode plus proche voisin polluant par polluant
 
-# ecrire le résultat dans un fichier csv
-Y_test_NO2.to_csv('data/Y_test.csv', index = False);
+Y_test_NO2 = nearestNeighbour('NO2', 3, train, X_test)
+Y_test_PM10 = nearestNeighbour('PM10', 3, train, X_test)
+Y_test_PM2_5 = nearestNeighbour('PM2_5', 3, train, X_test)
+Y_test = pd.concat([Y_test_NO2, Y_test_PM10, Y_test_PM2_5]);
+
+Y_test.to_csv('data/Y_test.csv', index = False);
