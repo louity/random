19d25ad86c5a30a8afaf06abad60f7a2db2c3eb7
Louis THIRY <louis.thiry@outlook.fr>
2016-12-03 11:04:10 +0100

    remove useless lines

--- src/main.py
+++ src/main.py
@@ -13,27 +13,10 @@ import plotUtils
 import nearestNeighbors
 import preprocessing
 import verification
+import loadData
 
-# lire les données
-X_train = pd.read_csv('data/X_train.csv')
-Y_train = pd.read_csv('data/Y_train.csv')
-X_test = pd.read_csv('data/X_test.csv')
+#nearestNeighbors.nearest_neighbors_by_pollutant()
+#nearestNeighbors.nearest_neighbors_by_pollutant_and_zone()
+#zone_station_pollutant_datas = preprocessing.separateZoneStationAndPollutantDatas(loadData.XY_train)
+#plotUtils.plot_zone_station_values(zone_station_pollutant_datas, 1.0, 'NO2')
 
-# fusionner X et Y en un seul dataframe
-XY_train = pd.concat([X_train, Y_train['TARGET']], axis=1)
-
-# repérer les noms des différents polluants
-pollutant_train_datas = preprocessing.separatePollutantDatas(XY_train, True)
-pollutant_test_datas = preprocessing.separatePollutantDatas(X_test, True)
-
-# methode plus proche voisin polluant par polluant
-n_neighbors = 4
-Y_test = pd.DataFrame();
-
-for pollutant in pollutant_train_datas:
-    pollutant_train_data = pollutant_train_datas[pollutant]
-    pollutant_test_data = pollutant_test_datas[pollutant]
-    Y_test_pollutant = nearestNeighbors.nearestNeighbors(pollutant_train_data, pollutant_test_data, n_neighbors)
-    Y_test = pd.concat([Y_test, Y_test_pollutant])
-
-Y_test.to_csv('data/Y_test.csv', index=False)
