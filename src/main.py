55761aa93bf34ce73b05c6fcb569d7bf66cbe4a9
Louis THIRY <louis.thiry@outlook.fr>
2016-12-08 10:49:32 +0100

    test knn with new temporal values

--- src/main.py
+++ src/main.py
@@ -15,8 +15,11 @@ import preprocessing
 import verification
 import loadData
 
+XY_train = preprocessing.addTemporalValues(loadData.XY_train)
+X_test = preprocessing.addTemporalValues(loadData.X_test)
 #nearestNeighbors.nearest_neighbors_by_pollutant()
 #nearestNeighbors.nearest_neighbors_by_pollutant_and_zone()
+nearestNeighbors.nearest_neighbors_by_pollutant(train_data=XY_train, test_data=X_test)
 #zone_station_pollutant_datas = preprocessing.separateZoneStationAndPollutantDatas(loadData.XY_train)
 #plotUtils.plot_zone_station_values(zone_station_pollutant_datas, 1.0, 'NO2')
 
